f5e22f35b758a39088b5d30132f826201c43b61a
narjissek <82451448+narjissek@users.noreply.github.com>
2021-08-30 14:10:55 +0200

    js

--- front/index.js
+++ front/index.js
@@ -1,27 +1,29 @@
 const api = 'http://localhost:3000/api/teddies';
 
+console.log("hello");
+
 const insertArticle = async () => {
 
-    const articles = await fetch('${api}').json();
+    // const articles = await fetch('${api}').json();
+    const products = await fetch('${api}').then(result => result.json());
 
-    console.log(article);
+    console.log(products);
 
-    for (let article of articles) {
+     for (let product of products) {
 
-        document.getElementById("app").innerHTML = 
+         document.getElementById("app").innerHTML += 
+        '<a href="produit.html?id=${product._id}' 
+         class="card" style="width: 18rem;">
+                 <img src="${article.imageUrl}" class="card-img-top" alt="${article.name}">
+                 <div class="card-body" id="article">
+                     <h5 class="card-title" id="article-titre">${article.name}</h5>
+                     <p class="card-text" id="article-description">${article.description}</p>
+                     <p class="font-weight-bold card-text"><span id="article-prix">${article.price}</span>€</p>
+                 </div>     
+         </a>';
         
-        '<div class="mx-auto px-5">
-            <div class="card" style="width: 18rem;">
-                <img src="${article.imageUrl}" class="card-img-top" alt="${article.name}">
-                <div class="card-body" id="article">
-                    <h5 class="card-title" id="article-titre">${article.name}</h5>
-                    <p class="card-text" id="article-description">${article.description}</p>
-                    <p class="font-weight-bold card-text"><span id="article-prix">${article.price}</span>€</p>
-                </div>
-            </div>
-        </div>'
-        
-    }
-}
+     }
+};
 
-insertArticle();
+
+ insertProducts();
