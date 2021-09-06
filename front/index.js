ca2dfed1aac441665a74cd7b546cff8e4340a29a
narjissek <82451448+narjissek@users.noreply.github.com>
2021-09-06 23:41:57 +0200

    update

--- front/index.js
+++ front/index.js
@@ -12,7 +12,7 @@ const insertProducts = async () => {
 
       for (let product of products) {
          document.getElementById("app").innerHTML += 
-         `<a class="link-dark p-2 d-flex align-items-stretch justify-content-center" href="produit.html?id=${product._id}"> <div class="card " style="width: 18rem;"> 
+         `<a class="link-dark p-2 px-5 d-flex align-items-stretch justify-content-center" href="produit.html?id=${product._id}"> <div class="card " style="width: 18rem;"> 
                  <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}"> 
                  <div class="card-body" id="article"> 
                      <h5 class="card-title" id="article-titre">${product.name}</h5>
